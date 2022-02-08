d8d633e54d2deefa4920d5eca047d79b525d1fb5
Silvia Dimol <silviadmolina@gmail.com>
2022-02-08 11:31:27 +0100

    add new users

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const movies = require("../web/src/data/movies.json");
 const users = require("../web/src/data/users.json");
 const Database = require("better-sqlite3");
 const db = new Database("./src/db/database.db", { verbose: console.log });
+const dbusers = new Database("./src/db/dataBaseUsers.db", { verbose: console.log });
 
 // create and config server
 const server = express();
@@ -41,6 +42,20 @@ server.get("/movie/:movieId", (req, res) => {
   const query = db.prepare("SELECT * FROM movies WHERE id = ?");
   const foundMovie = query.get(req.params.movieId);
   res.render("movie", foundMovie);
+});
+
+// POST, registro de nuevas usuarias en el back
+server.post("/signup", (req, res) => {
+  const query = dbusers.prepare("SELECT * FROM users WHERE email = ?");
+  const result = query.run(req.body.email);
+  res.json(result);
+  // const query = dbusers.prepare("INSERT INTO users (email, password) VALUES (?, ?)");
+  // const result = query.run(req.body.email, req.body.password);
+  // console.log(result);
+  // res.json({
+  //   "success": true,
+  //   "userId": result.lastInsertRowid
+  // });
 });
 
 //escribimos la ruta con ./src porque node busca la carpeta de estáticos desde la raiz del proyecto
