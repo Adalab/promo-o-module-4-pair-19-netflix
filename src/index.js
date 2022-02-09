aa7a1985df1085a9e1d894409af64584d7956e99
mariarguezp <mariarguezp5@gmail.com>
2022-02-09 11:43:06 +0100

    add login endpoint, create rel_movies_users table, create /user/movies fetch and start creating endpoint

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ const movies = require("../web/src/data/movies.json");
 const users = require("../web/src/data/users.json");
 const Database = require("better-sqlite3");
 const db = new Database("./src/db/database.db", { verbose: console.log });
-const dbusers = new Database("./src/db/dataBaseUsers.db", { verbose: console.log });
 
 // create and config server
 const server = express();
@@ -29,6 +28,25 @@ server.get("/movies", (req, res) => {
   });
 });
 
+server.post("/login", (req, res) => {
+  console.log("Petición a la ruta POST /login");
+  const query = db.prepare(
+    "SELECT * FROM users WHERE email = ? and password = ?"
+  );
+  const user = query.get(req.body.email, req.body.password);
+  if (user !== undefined) {
+    res.json({
+      success: true,
+      userId: "id_de_la_usuaria_encontrada",
+    });
+  } else {
+    res.json({
+      success: false,
+      errorMessage: "Usuaria/o no encontrada/o",
+    });
+  }
+});
+
 // A revisar!
 // server.post("/users", (req, res) => {
 //   console.log("req.body");
@@ -46,16 +64,26 @@ server.get("/movie/:movieId", (req, res) => {
 
 // POST, registro de nuevas usuarias en el back
 server.post("/signup", (req, res) => {
-  const query = dbusers.prepare("SELECT * FROM users WHERE email = ?");
-  const result = query.run(req.body.email);
-  res.json(result);
-  // const query = dbusers.prepare("INSERT INTO users (email, password) VALUES (?, ?)");
-  // const result = query.run(req.body.email, req.body.password);
-  // console.log(result);
-  // res.json({
-  //   "success": true,
-  //   "userId": result.lastInsertRowid
-  // });
+  // const query = db.prepare("SELECT * FROM users WHERE email = ?");
+  // const result = query.run(req.body.email);
+  // res.json(result);
+  const query = db.prepare("INSERT INTO users (email, password) VALUES (?, ?)");
+  const result = query.run(req.body.email, req.body.password);
+  console.log(result);
+  res.json({
+    success: true,
+    userId: result.lastInsertRowid,
+  });
+});
+
+// Get obtener usuario y película
+server.get("/user/movies", (req, res) => {
+  const query = db.prepare("SELECT * FROM rel_movies_users WHERE id = ?");
+  const result = query.get(req.header("user-id"));
+  res.json({
+    success: true,
+    movies: [],
+  });
 });
 
 //escribimos la ruta con ./src porque node busca la carpeta de estáticos desde la raiz del proyecto
--- web/src/services/api-user.js
+++ web/src/services/api-user.js
@@ -21,16 +21,15 @@ const sendLoginToApi = (data) => {
 const sendSingUpToApi = (data) => {
   console.log("Se están enviando datos al signup:", data);
   // CAMBIA ESTE FETCH PARA QUE APUNTE A UN ENDPOINT DE TU SERVIDOR, PIENSA SI DEBE SER GET O POST, PIENSA QUÉ DATOS DEBES ENVIAR, ETC
-  return fetch(
-    "http://localhost:4000/signup", {
-      method: "POST",
-      body: JSON.stringify(data),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
+  return fetch("http://localhost:4000/signup", {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
     .then((response) => response.json())
-    .then(data => {
+    .then((data) => {
       // CAMBIA EL CONTENIDO DE ESTE THEN PARA GESTIONAR LA RESPUESTA DEL SERVIDOR Y RETORNAR AL COMPONENTE APP LO QUE NECESITA
       return data;
     });
@@ -72,24 +71,16 @@ const getUserMoviesFromApi = (userId) => {
     userId
   );
   // CAMBIA ESTE FETCH PARA QUE APUNTE A UN ENDPOINT DE TU SERVIDOR, PIENSA SI DEBE SER GET O POST, PIENSA QUÉ DATOS DEBES ENVIAR, ETC
-  return fetch(
-    "//beta.adalab.es/curso-intensivo-fullstack-recursos/apis/netflix-v1/empty.json"
-  )
+  return fetch("http://localhost:4000/user/movies", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      "user-id": userId,
+    },
+  })
     .then((response) => response.json())
-    .then(() => {
-      // CAMBIA EL CONTENIDO DE ESTE THEN PARA GESTIONAR LA RESPUESTA DEL SERVIDOR Y RETORNAR AL COMPONENTE APP LO QUE NECESITA
-      return {
-        success: true,
-        movies: [
-          {
-            id: 1,
-            title: "Gambita de dama",
-            gender: "Drama",
-            image:
-              "//beta.adalab.es/curso-intensivo-fullstack-recursos/apis/netflix-v1/images/gambito-de-dama.jpg",
-          },
-        ],
-      };
+    .then((data) => {
+      return data;
     });
 };
 
